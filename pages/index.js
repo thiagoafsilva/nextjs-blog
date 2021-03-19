ecae7a77d5a12fa42902964975955eee76cd989c
thiago silva <thiagoafsilva@gmail.com>
2021-03-18 23:00:56 -0300

    primeiro commit teste vercell

---
+++ pages/index.js
@@ -0,0 +1,29 @@
+import {useState} from 'react';
+
+function Home(){
+
+    return ( 
+        <div>
+            <h3>Home</h3>
+            <Contador />    
+        </div>
+
+        )
+}
+
+function Contador(){
+    const [contador, setContador] = useState(1);
+    
+    function adicionarContador(){
+        setContador(contador + 1);
+    }
+    return (
+        <div>
+            <div>{contador}</div>
+            <button onClick={adicionarContador}>Adcionar</button>
+        </div>
+    )
+
+}
+
+export default Home
